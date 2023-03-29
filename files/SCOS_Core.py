546805a4bddd92d90c41b43d53f6d576ffdd6479
hsc100zz@gmail.com <hsc100zz@gmail.com>
2023-03-29 19:16:17 +0200

    l

--- files/SCOS_Core.py
+++ files/SCOS_Core.py
@@ -4,9 +4,9 @@ def load(f):
 		out = file.readline()
 		if out == "exit":
 			exit()
-		exec(out)
 		if out == None:
 		  print("Got error: 1")
 		  input("Press enter to exit...")
 		  exit()
-
+print("SCOS Core Version 1.0b")
+exit()
